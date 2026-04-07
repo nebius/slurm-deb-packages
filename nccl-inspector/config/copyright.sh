443dc5670fe5904c69719bc7ee941e0a677fb8cb
Dmitry Starov <dmitry.staroff@gmail.com>
2026-04-07 15:50:21 +0200

    [ADD] nFPM config for NCCL Inspector deb package

---
+++ nccl-inspector/config/copyright.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+(
+  echo 'Format: https://www.debian.org/doc/packaging-manuals/copyright-format/1.0/'
+  echo 'Upstream-Name: NCCL Inspector Plugin'
+  echo 'Source: https://github.com/NVIDIA/nccl/tree/master/plugins/profiler/inspector'
+  echo
+  echo 'Files: *'
+  echo 'Copyright: All source code and accompanying documentation is copyright (c) 2015-2020, NVIDIA CORPORATION. All rights reserved.'
+  echo 'License: Apache-2.0'
+  echo 'Comment: This Debian package redistributes unmodified upstream build artifacts.'
+  echo
+  echo 'License: Apache-2.0'
+  sed 's/^/ /; s/^ $/./' ../../../LICENSE.txt
+) > copyright
